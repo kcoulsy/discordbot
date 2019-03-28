ed8ac4ec75cc256eea699022b02f12bf0488a65b
Kristian <kristian@coulsy.co.uk>
2019-03-28 19:24:06 +0000

    Big updates, most functionality done

--- index.js
+++ index.js
@@ -1,135 +1,182 @@
-const Discord = require('discord.js');
-const axios = require('axios');
+const Discord = require("discord.js");
+// const axios = require("axios");
 
-const botconfig = require('./botconfig.json');
+const botconfig = require("./botconfig.json");
+
+const { BotEvent } = require("./models/BotEvent");
 
 const bot = new Discord.Client();
 
+const RSVP_ACCEPT = "✅";
+const RSVP_MAYBE = "❔";
+const RSVP_DECLINE = "❌";
 
-const RSVP_ACCEPT = '✅';
-const RSVP_MAYBE = '❔';
-const RSVP_DECLINE = '❌';
+let classRoles = [
+  "Warrior",
+  "Paladin",
+  "Hunter",
+  "Druid",
+  "Rogue",
+  "Priest",
+  "Mage",
+  "Warlock"
+];
 
-bot.on('ready', () => {
+let specRoles = ["Tank", "Healer", "Damage"];
+
+let classRoleMap = {};
+let specRoleMap = {};
+
+let eventStore = [];
+
+let botEventStore = [];
+
+bot.on("ready", () => {
+  const guild = bot.guilds.find("name", "Prototype");
+
+  classRoles.forEach(roleName => {
+    let role = guild.roles.find("name", roleName);
+    if (role) classRoleMap[role.id] = roleName;
+  });
+
+  specRoles.forEach(roleName => {
+    let role = guild.roles.find("name", roleName);
+    if (role) specRoleMap[role.id] = roleName;
+  });
+
   console.log(`Logged in as ${bot.user.tag}!`);
-  bot.user.setActivity('#help');
+  bot.user.setActivity("#help");
 });
 
-bot.on('message', msg => {
+bot.on("message", msg => {
   let prefix = botconfig.prefix;
-  let msgArray = msg.content.split(' ');
+  let msgArray = msg.content.split(" ");
   let cmd = msgArray[0];
   let args = msgArray.slice(1);
 
-  switch(cmd){
+  switch (cmd) {
     case `${prefix}newevent`:
-    let embedMsg = `event
-    
-    :tank:
-
-    - :warrior:
-
-    :healer: 
-
-    - :paladin: 
-    - :priest: 
-    - :druid: 
-
-    :Damage: 
-
-    - :warrior:  
-    - :hunter:  
-    - :rogue:
-    - :mage:  
-    - :warlock:
-
-    Please react to this post with ${RSVP_ACCEPT} to **Accept**, ${RSVP_MAYBE} for **Maybe**, and ${RSVP_DECLINE} to **Decline**.`
-
+      eventStore.push({});
+      botEventStore.push(new BotEvent({ id: botEventStore.length }));
       let eventembed = new Discord.RichEmbed()
-        .addField(args.join(' '), embedMsg);
-       
-      msg.channel.send(eventembed).then((msg) => {
+      .setThumbnail('https://i.gyazo.com/5f9ed3d6298a3bc0b46f15808ec6a659.png')
+      .setColor(0x52A030)
+      .addField(
+        `Event #${eventStore.length - 1} - ${args.join(" ")}`,
+        generateMessage({})
+      )
+      msg.channel.send(eventembed).then(sentMsg => {
         Promise.all([
-          msg.react(RSVP_ACCEPT),
-          msg.react(RSVP_MAYBE),
-          msg.react(RSVP_DECLINE)
-        ])
+          sentMsg.react(RSVP_ACCEPT),
+          sentMsg.react(RSVP_MAYBE),
+          sentMsg.react(RSVP_DECLINE)
+        ]);
       });
       break;
-    case `${prefix}botinfo`:
-        let bicon = bot.user.displayAvatarURL;
-        let botembed = new Discord.RichEmbed()
-        .setDescription('Bot info')
-        .setColor('#64fc6f')
-        .setThumbnail(bicon)
-        .addField('Bot Name', bot.user.username)
-        .addField('This bot was created by Sytho-Draenor on ', bot.user.createdAt);
-        msg.channel.send(botembed);
-        break;
-
-    case `${prefix}serverinfo`:
-        let sicon = msg.guild.iconURL;
-        let serverembed = new Discord.RichEmbed()
-          .setDescription('Server Information')
-          .setColor('#64fc6f')
-          .setThumbnail(sicon)
-          .addField('Server Name', msg.guild.name)
-          .addField('Created on', msg.guild.createdAt)
-          .addField('You joined', msg.guild.joinedAt)
-          .addField('Total Members', msg.guild.memberCount);
-        msg.channel.send(serverembed);
-        break;
-
-      case `${prefix}dox`:
-        if(args.length != 3){
-          let errorembed = new Discord.RichEmbed()
-          .addField('Syntax Error', 'To use the dox command you must format as "#dox charname server region"');
-          msg.channel.send(errorembed);
-        } else {
-          let name = args[0];
-          let server = args[1];
-          let region = args[2];
-          const details = getCharacterInfo(name, server, region);
-
-          let raiderio = {};
-
-          const raiderioUrl = `https://raider.io/api/v1/characters/profile?region=${region}&realm=${server}&name=${name}&fields=raid_progression%2Cmythic_plus_scores%2Cprevious_mythic_plus_scores%2Cmythic_plus_best_runs`;
-          axios(raiderioUrl).then((response)=>{
-            if(response.status === 200) {
-              raiderio = response;
-
-              let mplus = {
-                currentScore: raiderio.data.mythic_plus_scores,
-                previousScore: raiderio.data.previous_mythic_plus_scores,
-                bestRuns: raiderio.data.mythic_plus_best_runs
-              }
-              let raidProg = raiderio.data.raid_progression;
-              let antorus = raidProg["antorus-the-burning-throne"];
-              let characterembed = new Discord.RichEmbed()
-              .addField('Character Info', `${raiderio.data.name} - ${raiderio.data.realm} (${raiderio.data.region}) ${raiderio.data.race} ${raiderio.data.class}`)
-              .setThumbnail(raiderio.data.thumbnail_url)
-              .addField('Antorus Progression', antorus.summary)
-              .addField('Mythic Plus Score', `Current: ${mplus.currentScore.all}\n` +
-                          `Tank: ${mplus.currentScore.tank}\n` +
-                          `Healer: ${mplus.currentScore.healer}\n` +
-                          `DPS: ${mplus.currentScore.dps}\n` +
-                          `Previous: ${mplus.previousScore.all}`)
-              .addField('Best Mythic Plus Runs', `${mplus.bestRuns[0].dungeon} - ${mplus.bestRuns[0].mythic_level} +${mplus.bestRuns[0].num_keystone_upgrades}\n` +
-                                                 `${mplus.bestRuns[1].dungeon} - ${mplus.bestRuns[1].mythic_level} +${mplus.bestRuns[1].num_keystone_upgrades}\n` +
-                                                 `${mplus.bestRuns[2].dungeon} - ${mplus.bestRuns[2].mythic_level} +${mplus.bestRuns[2].num_keystone_upgrades}`);
-              msg.channel.send(characterembed);
-            }
-        }).catch((e)=>{
-          let errorembed = new Discord.RichEmbed()
-          .addField('Error', 'Unable to retrieve information on that character');
-          msg.channel.send(errorembed);
-        });
-      }
     default:
       break;
   }
 });
-const getCharacterInfo = (name, server, region) => {
 
-}
+bot.on("messageReactionAdd", (reaction, user) => {
+  let title = reaction.message.embeds[0].fields[0].name;
+  let id = title.match(/#(\d+)/)[1];
+  if (user.bot) {
+    return;
+  }
+  reaction.message.guild.fetchMember(user.id).then(user => {
+
+    let userObj = {
+      id: user.id,
+      user,
+      playerClass: null,
+      playerRole: null
+    };
+    user._roles.forEach(roleId => {
+      if (specRoleMap[roleId]) {
+        userObj.playerRole = specRoleMap[roleId];
+      }
+      if (classRoleMap[roleId]) {
+        userObj.playerClass = classRoleMap[roleId];
+      }
+    });
+
+    switch (reaction._emoji.name) {
+      case RSVP_ACCEPT:
+        botEventStore[id].addPlayer(
+          userObj,
+          userObj.playerRole,
+          userObj.playerClass
+        );
+        break;
+      case RSVP_MAYBE:
+        botEventStore[id].addPlayer(userObj, "Maybe", userObj.playerClass);
+        break;
+      case RSVP_DECLINE:
+        botEventStore[id].addPlayer(userObj, "Declined", userObj.playerClass);
+        break;
+      default:
+        break;
+    }
+
+    reaction.message.edit(
+      new Discord.RichEmbed()
+      .setThumbnail('https://i.gyazo.com/5f9ed3d6298a3bc0b46f15808ec6a659.png')
+      .setColor(0x52A030)
+      .addField(
+        title,
+        generateMessage(botEventStore[id].store)
+      )
+      
+    );
+  });
+});
+
 bot.login(botconfig.token);
+
+const getClassIcon = className => {
+  return bot.emojis.find(emoji => emoji.name === className.toLowerCase());
+};
+
+const generateRoleMessage = role => {
+  if (!role) {
+    return "";
+  }
+  return Object.values(role).map(playerClass => {
+    if (!playerClass.length) return null;
+    return (
+      getClassIcon(playerClass[0].playerClass) +
+      playerClass.map(player => {
+        return player.user;
+      })
+    );
+  });
+};
+
+const generateMessage = accepted => {
+  const { Tank, Healer, Damage, Maybe, Declined } = accepted;
+
+  return `
+    __Players Going:__
+
+    **Tank**
+  ${generateRoleMessage(Tank)}
+    **Healer**
+  ${generateRoleMessage(Healer)}
+    **Damage**
+  ${generateRoleMessage(Damage)}
+
+  __Maybe__
+  ${generateRoleMessage(Maybe)}
+  __Declined__
+${generateRoleMessage(Declined)}
+  
+  Please react to this post with ${RSVP_ACCEPT} to **Accept**, ${RSVP_MAYBE} for **Maybe**, and ${RSVP_DECLINE} to **Decline**.`;
+};
+
+// TODO
+
+// REMOVE BOT FROM LIST (DON'T ALLOW IN FIRST PLACE)
+// ONLY ALLOW 1 CHOICE PER ID
+// ALLOW USER TO UNCHOOSE THEIR CHOICE
+// MAYBE SOME SORT OF DB.

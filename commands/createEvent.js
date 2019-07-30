4f5ad0737694c100bfc8a945fb8d33cb4e2ed6c4
Kristian <kristian@coulsy.co.uk>
2019-07-30 17:10:00 +0100

    Linting

--- commands/createEvent.js
+++ commands/createEvent.js
@@ -1,20 +1,20 @@
-const Discord = require('discord.js')
+const Discord = require('discord.js');
 
-const CONSTS = require('../constants/main')
-const raidMap = require('../constants/raidMap')
-const generateMessage = require('../utils/generateMessage')
+const CONSTS = require('../constants/main');
+const raidMap = require('../constants/raidMap');
+const generateMessage = require('../utils/generateMessage');
 
 module.exports = (bot, msg, store) => {
-    let msgArray = msg.content.split(' ')
-    let raid = msgArray[1]
-    let eventTitle = msgArray.slice(2).join(' ')
+    let msgArray = msg.content.split(' ');
+    let raid = msgArray[1];
+    let eventTitle = msgArray.slice(2).join(' ');
 
     if (!raidMap[raid]) {
         return msg.channel.send(
             `The format is \`$newevent {eventtype} {text}\` Valid eventtypes : ${Object.keys(
                 raidMap
             ).join(' ')}`
-        )
+        );
     }
 
     let eventembed = new Discord.RichEmbed()
@@ -23,7 +23,7 @@ module.exports = (bot, msg, store) => {
         .addField(
             `#Event ${raidMap[raid].name} - ${eventTitle}`,
             generateMessage(bot, {})
-        )
+        );
     bot.channels
         .find('name', 'events')
         .send(eventembed)
@@ -32,8 +32,8 @@ module.exports = (bot, msg, store) => {
                 sentMsg.react(CONSTS.EMOJI_ACCEPT),
                 sentMsg.react(CONSTS.EMOJI_MAYBE),
                 sentMsg.react(CONSTS.EMOJI_DECLINE),
-            ])
-            console.log(sentMsg.id)
+            ]);
+            console.log(sentMsg.id);
             store.dispatch({
                 id: sentMsg.id,
                 type: 'add_event',
@@ -42,6 +42,6 @@ module.exports = (bot, msg, store) => {
                     event: raidMap[raid],
                     attending: [],
                 },
-            })
-        })
-}
+            });
+        });
+};
